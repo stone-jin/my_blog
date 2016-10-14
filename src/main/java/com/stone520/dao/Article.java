1cdd7b994f2efdb6a80d394ea63e551f91b6fdd8
stone-jin <hzjinbing@gmail.com>
2016-10-14 22:56:34 +0800

    基础：增加xhr ajax的接口模板，准备先调通发表文章的接口

---
+++ src/main/java/com/stone520/dao/Article.java
@@ -0,0 +1,18 @@
+package com.stone520.dao;
+
+/**
+ * Description:
+ * Author: stone-jin
+ * Date: 2016/10/14
+ */
+public class Article {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/stone520/model/ArticleInfo.java
@@ -0,0 +1,18 @@
+package com.stone520.model;
+
+/**
+ * Description:
+ * Author: stone-jin
+ * Date: 2016/10/14
+ */
+public class ArticleInfo {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
